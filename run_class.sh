d8229b50100be415b1f3e9d65688005217773087
Jiang Lekang <lj408@cam.ac.uk>
2023-03-19 23:42:56 +0000

    remove

--- run_class.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-data=UNtrade
-n_runs=1
-
-# TGN
-method=tgn
-prefix="${method}_attn"
-python tgn/train_tgn_classification.py -d $data --use_memory --prefix "$prefix" --n_runs "$n_runs" --gpu 0 --n_epoch 30 --num_class 3
